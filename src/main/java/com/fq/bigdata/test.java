6d62fcf24b4349d24d16aff0f2bd9e81f207f79e
KinaPig <2877808194@qq.com>
2018-11-06 20:25:19 +0800

    test

---
+++ src/main/java/com/fq/bigdata/test.java
@@ -0,0 +1,11 @@
+package com.fq.bigdata;
+
+/**
+ * @auther fq
+ * @DATE 2018/11/6 20:23
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test!!!");
+    }
+}
